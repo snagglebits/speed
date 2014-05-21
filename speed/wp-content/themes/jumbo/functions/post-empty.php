abd54abd09ae3771474a94aa4a2fb73e3112fa16
Peter Levine <peterlevine@peters-imac.gateway.2wire.net>
2014-05-20 18:16:25 -0700

    deleted unnecessary speed folder

--- speed/wp-content/themes/jumbo/functions/post-empty.php
+++
@@ -1,6 +0,0 @@
-<li class="post">
-	<h2 class="post-title"><a href="#"><?php _e("No Content Found", "ocmx"); ?></a></h2>
-	<div class="copy">
-		<?php _e("The content you are looking for does not exist", "ocmx"); ?>
-	</div>
-</li>
--- speed/wp-content/themes/jumbo/ocmx/theme-setup/setup.php
+++
@@ -1,4 +0,0 @@
-<?php 
-function check_gallery_table(){ /* Deprecated */ }
-function check_table_existance($new_table) { /* Deprecated */ }
-?>
