92eb6e30a5a316215e050c01f2357a8f961f1add
snagglebits <j.reed.snavely@gmail.com>
2014-05-20 18:02:07 -0700

    Moved folder on MBP

--- wp-content/themes/jumbo/ajax/remove-image.php
+++
@@ -1,5 +0,0 @@
-<?php function ocmx_ajax_remove_image(){
-	wp_delete_attachment( $_GET["attachid"]);
-	die("");
-}
-?>
--- wp-content/themes/jumbo/ocmx/interface/widgets.php
+++
@@ -1,8 +0,0 @@
-<?php    
-
-function ocmx_deregister_woo_widgets(){
-	unregister_widget('WooCommerce_Widget_Layered_Nav');
-}
-add_action('widgets_init', 'ocmx_deregister_woo_widgets');
-
-?>
