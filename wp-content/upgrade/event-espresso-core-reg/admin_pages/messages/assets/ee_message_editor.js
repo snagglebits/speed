a1ae28fb8cba0e8a194ccb054d45e9ef08cbe37f
Justin Snavely <traumahealing@gmail.com>
2014-11-03 13:02:12 -0800

    Updated Akismet, Math Captcha, and changed the payment methods pic for EE from the image I used on SE.com

--- wp-content/upgrade/event-espresso-core-reg/admin_pages/messages/assets/ee_message_editor.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-	$('#mtp_extra_actions').on('click', '.reset-default-button', function(e) {
-		var reset = confirm(eei18n.confirm_default_reset);
-		if ( reset ) return true;
-		e.preventDefault();
-	});
-});
--- wp-content/upgrade/event-espresso-core-reg/core/db_models/fields/EE_DB_Only_Text_Field.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once( EE_MODELS . 'fields/EE_DB_Only_Field_Base.php');
-class EE_DB_Only_Text_Field extends EE_DB_Only_Field_Base{
-	function get_wpdb_data_type(){
-		return '%s';
-	}
-}
